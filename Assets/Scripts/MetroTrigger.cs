7281c1e3ff9563a64c44d3e9705b32f721ddb5fa
Adilkhan-111 <gulzhanyessetova@gmail.com>
2025-08-27 21:53:51 +0500

    initial commit
    METRO IS COMPLETED

---
+++ Assets/Scripts/MetroTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MetroTrigger : MonoBehaviour
+{
+    [SerializeField] private string sceneToLoad;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
